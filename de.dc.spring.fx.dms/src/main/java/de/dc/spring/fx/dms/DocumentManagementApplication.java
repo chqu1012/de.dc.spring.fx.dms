3f01d5777d19e04fc2ed55ae158e6ff11f7d6b15
Dim <Dim@Duye>
2018-02-27 23:11:25 +0100

    DMS-00001: Initial imports

---
+++ de.dc.spring.fx.dms/src/main/java/de/dc/spring/fx/dms/DocumentManagementApplication.java
@@ -0,0 +1,49 @@
+package de.dc.spring.fx.dms; 
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+@SpringBootApplication
+public class DocumentManagementApplication extends Application {
+
+    private ConfigurableApplicationContext springContext;
+    private AnchorPane root;
+	private FXMLLoader fxmlLoader;
+
+    @Override
+    public void init() throws Exception {
+        springContext = SpringApplication.run(DocumentManagementApplication.class);
+        fxmlLoader = new FXMLLoader(getClass().getResource("DMSMain.fxml"));
+        fxmlLoader.setControllerFactory(springContext::getBean);
+        root = fxmlLoader.load();
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Phima Application");
+        Scene scene = new Scene(root, 1230, 800);
+        scene.setFill(Color.TRANSPARENT);
+        primaryStage.initStyle(StageStyle.DECORATED);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    @Override
+    public void stop() throws Exception {
+        springContext.stop();
+    }
+
+
+    public static void main(String[] args) {
+        launch(DocumentManagementApplication.class);
+    }
+}
