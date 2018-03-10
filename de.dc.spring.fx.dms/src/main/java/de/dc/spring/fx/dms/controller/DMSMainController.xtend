88ba5185a673c7b9ab12296c76c4dc29283fe6df
Dim <Dim@Duye>
2018-03-10 23:52:40 +0100

    DMS-00040: Migrated xtend (DMSMainController)

---
+++ de.dc.spring.fx.dms/src/main/java/de/dc/spring/fx/dms/controller/DMSMainController.xtend
@@ -0,0 +1,113 @@
+package de.dc.spring.fx.dms.controller
+
+import de.dc.fx.animation.other.AnimationType
+import de.dc.fx.animation.other.AnimationUtils
+import de.dc.spring.fx.dms.model.Ticket
+import de.dc.spring.fx.dms.service.TicketService
+import de.dc.spring.fx.dms.util.FolderUtil
+import javafx.fxml.FXML
+import javafx.scene.Parent
+import javafx.scene.input.KeyCode
+import javafx.scene.input.KeyEvent
+import javafx.scene.input.MouseEvent
+import org.controlsfx.control.textfield.TextFields
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Controller
+
+@Controller class DMSMainController extends BaseDMSMainController {
+	
+	@FXML HomeTileController invoiceAnchorPaneController
+	@FXML HomeTileController howtoAnchorPaneController
+	@FXML HomeTileController formularAnchorPaneController
+	@FXML HomeTileController receiptAnchorPaneController
+	
+	Parent lastPanel
+	
+	@Autowired TicketService ticketService
+	@Autowired DMSDetailController dmsDetailController
+	@Autowired FolderUtil folderUtil
+
+	def initialize() {
+		invoiceAnchorPaneController.imageBackground = "#0F62C6"
+		howtoAnchorPaneController.imageBackground = "#009AD2"
+		formularAnchorPaneController.imageBackground = "#00A0A3"
+		receiptAnchorPaneController.imageBackground = "#FF8350"
+		lastPanel = homePanel
+		folderUtil.createIfNotExist
+		// TODO: Fill with DB data
+		TextFields.bindAutoCompletion(searchText, ticketService.all)
+	}
+
+	override onSwitchToAddDocument(MouseEvent event) {
+		currentPaneLabel.text = "New Document"
+		addDocumentPanel.toFront
+		AnimationUtils.createTransition(lastPanel, AnimationType.FADE_OUT_DOWN).play
+		lastPanel = addDocumentPanel
+		AnimationUtils.createTransition(addDocumentPanel, AnimationType.FADE_IN_LEFT).play
+	}
+
+	override onSwitchToExit(MouseEvent event) {
+		currentPaneLabel.text = "Exit"
+	}
+
+	override onSwitchToHome(MouseEvent event) {
+		currentPaneLabel.text = "Dashboard"
+		homePanel.toFront
+		AnimationUtils.createTransition(lastPanel, AnimationType.FADE_OUT_DOWN).play
+		lastPanel = homePanel
+		AnimationUtils.createTransition(homePanel, AnimationType.FADE_IN_LEFT).play
+	}
+
+	override onSwitchToLogOut(MouseEvent event) {
+		currentPaneLabel.text = "Log Out"
+	}
+
+	override onSwitchToPreferences(MouseEvent event) {
+		currentPaneLabel.text = "Preferences"
+	}
+
+	override onSwitchToViewDocuments(MouseEvent event) {
+		currentPaneLabel.text = "Documents View"
+		viewDocumentsPanel.toFront
+		AnimationUtils.createTransition(lastPanel, AnimationType.FADE_OUT_DOWN).play
+		lastPanel = viewDocumentsPanel
+		AnimationUtils.createTransition(viewDocumentsPanel, AnimationType.FADE_IN_LEFT).play
+	}
+
+	override onSwitchToCalendar(MouseEvent event) {
+		currentPaneLabel.text = "Calendar"
+		calendarPane.toFront
+		AnimationUtils.createTransition(lastPanel, AnimationType.FADE_OUT_DOWN).play
+		lastPanel = calendarPane
+		AnimationUtils.createTransition(calendarPane, AnimationType.FADE_IN_LEFT).play
+	}
+
+	@FXML def onSearchTextKeyReleased(KeyEvent event) {
+		if (event.getCode.equals(KeyCode.ENTER)) {
+			var searchFields = searchText.text.split(":")
+			var splittedId = searchFields.get(0).split("-").get(1)
+			var id = Long.valueOf(splittedId)
+			var tickets = ticketService.findById(id)
+			tickets.get.showTicket
+			searchText.text = ""
+		}
+	}
+
+	def showTicket(Ticket ticket) {
+		currentPaneLabel.text = "Document Details"
+		dmsDetailPane.toFront
+		AnimationUtils.createTransition(lastPanel, AnimationType.FADE_OUT_DOWN).play
+		lastPanel = dmsDetailPane
+		dmsDetailController.setDetails(ticket)
+		AnimationUtils.createTransition(dmsDetailPane, AnimationType.FADE_IN_LEFT).play
+		dmsDetailPane.toFront
+	}
+
+	override onSwitchToHelp(MouseEvent event) {
+		currentPaneLabel.text = "Help"
+	}
+
+	override onSwitchToReport(MouseEvent event) {
+		currentPaneLabel.text = "Report"
+	}
+}
---
+++ de.dc.spring.fx.dms/src/main/xtend-gen/de/dc/spring/fx/dms/controller/DMSMainController.java
@@ -0,0 +1,145 @@
+package de.dc.spring.fx.dms.controller;
+
+import de.dc.fx.animation.other.AnimationType;
+import de.dc.fx.animation.other.AnimationUtils;
+import de.dc.spring.fx.dms.controller.BaseDMSMainController;
+import de.dc.spring.fx.dms.controller.DMSDetailController;
+import de.dc.spring.fx.dms.controller.HomeTileController;
+import de.dc.spring.fx.dms.model.Ticket;
+import de.dc.spring.fx.dms.service.TicketService;
+import de.dc.spring.fx.dms.util.FolderUtil;
+import java.util.Optional;
+import javafx.fxml.FXML;
+import javafx.scene.Parent;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import org.controlsfx.control.textfield.AutoCompletionBinding;
+import org.controlsfx.control.textfield.TextFields;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@SuppressWarnings("all")
+public class DMSMainController extends BaseDMSMainController {
+  @FXML
+  private HomeTileController invoiceAnchorPaneController;
+  
+  @FXML
+  private HomeTileController howtoAnchorPaneController;
+  
+  @FXML
+  private HomeTileController formularAnchorPaneController;
+  
+  @FXML
+  private HomeTileController receiptAnchorPaneController;
+  
+  private Parent lastPanel;
+  
+  @Autowired
+  private TicketService ticketService;
+  
+  @Autowired
+  private DMSDetailController dmsDetailController;
+  
+  @Autowired
+  private FolderUtil folderUtil;
+  
+  public AutoCompletionBinding<Ticket> initialize() {
+    AutoCompletionBinding<Ticket> _xblockexpression = null;
+    {
+      this.invoiceAnchorPaneController.setImageBackground("#0F62C6");
+      this.howtoAnchorPaneController.setImageBackground("#009AD2");
+      this.formularAnchorPaneController.setImageBackground("#00A0A3");
+      this.receiptAnchorPaneController.setImageBackground("#FF8350");
+      this.lastPanel = this.homePanel;
+      this.folderUtil.createIfNotExist();
+      _xblockexpression = TextFields.<Ticket>bindAutoCompletion(this.searchText, this.ticketService.getAll());
+    }
+    return _xblockexpression;
+  }
+  
+  @Override
+  public void onSwitchToAddDocument(final MouseEvent event) {
+    this.currentPaneLabel.setText("New Document");
+    this.addDocumentPanel.toFront();
+    AnimationUtils.createTransition(this.lastPanel, AnimationType.FADE_OUT_DOWN).play();
+    this.lastPanel = this.addDocumentPanel;
+    AnimationUtils.createTransition(this.addDocumentPanel, AnimationType.FADE_IN_LEFT).play();
+  }
+  
+  @Override
+  public void onSwitchToExit(final MouseEvent event) {
+    this.currentPaneLabel.setText("Exit");
+  }
+  
+  @Override
+  public void onSwitchToHome(final MouseEvent event) {
+    this.currentPaneLabel.setText("Dashboard");
+    this.homePanel.toFront();
+    AnimationUtils.createTransition(this.lastPanel, AnimationType.FADE_OUT_DOWN).play();
+    this.lastPanel = this.homePanel;
+    AnimationUtils.createTransition(this.homePanel, AnimationType.FADE_IN_LEFT).play();
+  }
+  
+  @Override
+  public void onSwitchToLogOut(final MouseEvent event) {
+    this.currentPaneLabel.setText("Log Out");
+  }
+  
+  @Override
+  public void onSwitchToPreferences(final MouseEvent event) {
+    this.currentPaneLabel.setText("Preferences");
+  }
+  
+  @Override
+  public void onSwitchToViewDocuments(final MouseEvent event) {
+    this.currentPaneLabel.setText("Documents View");
+    this.viewDocumentsPanel.toFront();
+    AnimationUtils.createTransition(this.lastPanel, AnimationType.FADE_OUT_DOWN).play();
+    this.lastPanel = this.viewDocumentsPanel;
+    AnimationUtils.createTransition(this.viewDocumentsPanel, AnimationType.FADE_IN_LEFT).play();
+  }
+  
+  @Override
+  public void onSwitchToCalendar(final MouseEvent event) {
+    this.currentPaneLabel.setText("Calendar");
+    this.calendarPane.toFront();
+    AnimationUtils.createTransition(this.lastPanel, AnimationType.FADE_OUT_DOWN).play();
+    this.lastPanel = this.calendarPane;
+    AnimationUtils.createTransition(this.calendarPane, AnimationType.FADE_IN_LEFT).play();
+  }
+  
+  @FXML
+  public void onSearchTextKeyReleased(final KeyEvent event) {
+    boolean _equals = event.getCode().equals(KeyCode.ENTER);
+    if (_equals) {
+      String[] searchFields = this.searchText.getText().split(":");
+      String splittedId = searchFields[0].split("-")[1];
+      Long id = Long.valueOf(splittedId);
+      Optional<Ticket> tickets = this.ticketService.findById(id);
+      this.showTicket(tickets.get());
+      this.searchText.setText("");
+    }
+  }
+  
+  public void showTicket(final Ticket ticket) {
+    this.currentPaneLabel.setText("Document Details");
+    this.dmsDetailPane.toFront();
+    AnimationUtils.createTransition(this.lastPanel, AnimationType.FADE_OUT_DOWN).play();
+    this.lastPanel = this.dmsDetailPane;
+    this.dmsDetailController.setDetails(ticket);
+    AnimationUtils.createTransition(this.dmsDetailPane, AnimationType.FADE_IN_LEFT).play();
+    this.dmsDetailPane.toFront();
+  }
+  
+  @Override
+  public void onSwitchToHelp(final MouseEvent event) {
+    this.currentPaneLabel.setText("Help");
+  }
+  
+  @Override
+  public void onSwitchToReport(final MouseEvent event) {
+    this.currentPaneLabel.setText("Report");
+  }
+}
